89232e23abdcf9692ba3d7896398a97e6a57eff1
davidflast <davidflasterstein@wustl.edu>
2016-05-19 01:59:49 -0400

    stateData
    Added csv with state data

--- app/src/main/java/bracket/State.java
+++ app/src/main/java/bracket/State.java
@@ -4,16 +4,41 @@ package bracket;
  * Created by davidflast on 5/17/16.
  */
 public class State extends Team{
-    double population;
-    double percentTrump;
-    public State(String name, double population){
+    protected int percentObama;
+    protected int percentRomney;
+    protected int percentWhite;
+    protected int percentHispanic;
+    protected int percentBlack;
+    protected int electoralVotes;
+
+    public State(String name, int electoralVotes,
+                 int percentObama, int percentRomney, int percentWhite,
+                 int percentHispanic, int percentBlack) {
         super(name);
-        this.population = population;
+        this.electoralVotes = electoralVotes;
+        this.percentObama = percentObama;
+        this.percentRomney = percentRomney;
+        this.percentWhite = percentWhite;
+        this.percentHispanic = percentHispanic;
+        this.percentBlack = percentBlack;
     }
-    public double getScore(){
-        return this.percentTrump;
+
+    public int getPercentObama() {
+        return percentObama;
     }
-
-
-
+    public int getPercentRomney() {
+        return percentRomney;
+    }
+    public int getPercentWhite() {
+        return percentWhite;
+    }
+    public int getPercentHispanic() {
+        return percentHispanic;
+    }
+    public int getPercentBlack() {
+        return percentBlack;
+    }
+    public int getElectoralVotes() {
+        return electoralVotes;
+    }
 }
